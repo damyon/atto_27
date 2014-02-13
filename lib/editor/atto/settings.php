f669aa599bd2ee81af5da01291dd86b1ed8ad9f8
Frederic Massart <fred@moodle.com>
2014-02-13 12:14:13 +0800

    MDL-44125 atto_align: New plugin for text alignment

--- lib/editor/atto/settings.php
+++ lib/editor/atto/settings.php
@@ -36,6 +36,7 @@ if ($ADMIN->fulltree) {
                'links = link, unlink' . "\n" .
                'files = image, media, managefiles' . "\n" .
                'style2 = underline, strike, subscript, superscript' . "\n" .
+               'align = align' . "\n" .
                'indent = indent, outdent' . "\n" .
                'insert = charmap, table, clear' . "\n" .
                'accessibility = accessibilitychecker, accessibilityhelper' . "\n" .
